52e9304af3610e52a78e60207dffb962ff31b3c9
小朋 <2810580330@qq.com>
2020-03-22 04:06:07 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -22,5 +22,5 @@ iptables -t nat -A CLASHRULE -p tcp -j REDIRECT --to-ports 7892
 # 在 PREROUTING 链前插入 CLASHRULE 链,使其生效
 iptables -t nat -A PREROUTING -p tcp -j CLASHRULE
 #启动ssh
-/etc/init.d/ssh start
+service ssh restart
 /clash/clash-linux-armv8 -d /clash/
