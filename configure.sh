47e40bae10c0981d3e8ff21a3605dc19db562af0
小朋 <2810580330@qq.com>
2020-04-15 11:28:51 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -3,12 +3,9 @@
 sed -i "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g" /etc/sysctl.conf
 sysctl -p
 iptables -t nat -N CLASHRULE
-
 iptables -t nat -A CLASHRULE -d 127.0.0.0/8 -j RETURN
 iptables -t nat -A CLASHRULE -d 192.168.0.0/16 -j RETURN
 iptables -t nat -A CLASHRULE -p tcp -j REDIRECT --to-ports 7892
-
-# 在 PREROUTING 链前插入 CLASHRULE 链,使其生效
 iptables -t nat -A PREROUTING -p tcp -j CLASHRULE
 #启动ssh
 service ssh restart
