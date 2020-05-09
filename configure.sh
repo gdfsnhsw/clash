3ab0b03c4548a9db04e2f451d56561ec67107038
小朋 <2810580330@qq.com>
2020-05-10 02:48:59 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -6,6 +6,7 @@ iptables -t nat -N CLASHRULE
 iptables -t nat -A CLASHRULE -d 127.0.0.0/8 -j RETURN
 iptables -t nat -A CLASHRULE -d 192.168.0.0/16 -j RETURN
 iptables -t nat -A CLASHRULE -d 172.17.0.0/24 -j RETURN
+iptables -t nat -A CLASHRULE -d 169.254.0.0/16 -j RETURN
 iptables -t nat -A CLASHRULE -p tcp -j REDIRECT --to-ports 7892
 iptables -t nat -A PREROUTING -p tcp -j CLASHRULE
 #启动ssh
