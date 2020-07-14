4d337808ea7cb134243d2cc84c977d5674994290
小朋 <2810580330@qq.com>
2020-07-14 16:16:55 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -20,6 +20,8 @@ iptables -t nat -A clash -d 127.0.0.0/8 -j RETURN
 iptables -t nat -A clash -d 169.254.0.0/16 -j RETURN
 iptables -t nat -A clash -d 172.16.0.0/12 -j RETURN
 iptables -t nat -A clash -d 192.168.0.0/16 -j RETURN
+iptables -t nat -A clash -d 224.0.0.0/4 -j RETURN
+iptables -t nat -A clash -d 240.0.0.0/4 -j RETURN
 iptables -t nat -A clash -p tcp -j REDIRECT --to-port 7892
 
 iptables -t nat -A PREROUTING -p tcp -j clash
@@ -34,7 +36,8 @@ iptables -t mangle -A clash -d 127.0.0.0/8 -j RETURN
 iptables -t mangle -A clash -d 169.254.0.0/16 -j RETURN
 iptables -t mangle -A clash -d 172.16.0.0/12 -j RETURN
 iptables -t mangle -A clash -d 192.168.0.0/16 -j RETURN
-
+iptables -t mangle -A clash -d 224.0.0.0/4 -j RETURN
+iptables -t mangle -A clash -d 240.0.0.0/4 -j RETURN
 iptables -t mangle -A clash -p udp -j TPROXY --on-port 7892 --tproxy-mark 1
 iptables -t mangle -A PREROUTING -p udp -j clash
 
