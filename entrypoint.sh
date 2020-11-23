51f93fd5e059168f298d6b2df4b3add265677788
小朋 <2810580330@qq.com>
2020-11-23 18:51:00 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,7 +21,7 @@ iptables -t nat -A clash -d 172.16.0.0/12 -j RETURN
 iptables -t nat -A clash -d 192.168.0.0/16 -j RETURN
 iptables -t nat -A clash -d 224.0.0.0/4 -j RETURN
 iptables -t nat -A clash -d 240.0.0.0/4 -j RETURN
-iptables -t nat -A clash -p tcp -j REDIRECT --to-port 7893
+iptables -t nat -A clash -p tcp -j REDIRECT --to-port 7892
 iptables -t nat -A PREROUTING -p tcp -j clash
 
 #udp
