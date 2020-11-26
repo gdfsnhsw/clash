5ea9fe4afdd90b9684a631f084661fcdb7dbdd89
小朋 <2810580330@qq.com>
2020-11-27 02:16:58 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,19 +11,6 @@ iptables -t mangle -F
 iptables -F
 iptables -X
 
-#tcp
-iptables -t nat -N clash
-iptables -t nat -A clash -d 0.0.0.0/8 -j RETURN
-iptables -t nat -A clash -d 10.0.0.0/8 -j RETURN
-iptables -t nat -A clash -d 127.0.0.0/8 -j RETURN
-iptables -t nat -A clash -d 169.254.0.0/16 -j RETURN
-iptables -t nat -A clash -d 172.16.0.0/12 -j RETURN
-iptables -t nat -A clash -d 192.168.0.0/16 -j RETURN
-iptables -t nat -A clash -d 224.0.0.0/4 -j RETURN
-iptables -t nat -A clash -d 240.0.0.0/4 -j RETURN
-iptables -t nat -A clash -p tcp -j REDIRECT --to-port 7892
-iptables -t nat -A PREROUTING -p tcp -j clash
-
 #udp
 ip rule add fwmark 1 table 100
 ip route add local default dev lo table 100
@@ -36,9 +23,11 @@ iptables -t mangle -A clash -d 172.16.0.0/12 -j RETURN
 iptables -t mangle -A clash -d 192.168.0.0/16 -j RETURN
 iptables -t mangle -A clash -d 224.0.0.0/4 -j RETURN
 iptables -t mangle -A clash -d 240.0.0.0/4 -j RETURN
-iptables -t mangle -A clash -p tcp -j TPROXY --on-port 7892 --tproxy-mark 1
-iptables -t mangle -A PREROUTING -p udp -j clash
+iptables -t mangle -A clash -p tcp -j TPROXY --on-port 7893 --tproxy-mark 1
+iptables -t mangle -A clash -p udp -j TPROXY --on-port 7893 --tproxy-mark 1
+
+iptables -t mangle -A PREROUTING -j clash
 #由于国内dns会解析github到无法访问ip  添加hosts
 echo '192.30.255.113 github.com' >> /etc/hosts 
-#pm2-runtime clash
+#启动 clash
 clash
