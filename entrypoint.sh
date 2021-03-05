5db1169b0e4e49962383c827a2290d0308642a3e
小朋 <2810580330@qq.com>
2021-03-05 13:36:58 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,8 +27,6 @@ iptables -t mangle -A clash -p tcp -j TPROXY --on-port 7893 --tproxy-mark 1
 iptables -t mangle -A clash -p udp -j TPROXY --on-port 7893 --tproxy-mark 1
 iptables -t mangle -A PREROUTING -j clash
 if [ ! -e '/root/.config/clash/dashboard/index.html' ]; then
-    echo "删除旧版本dashboard面板"
-    rm -rf /root/.config/clash/dashboard
     echo "开始移动面板文件到dashboard目录"
     tar -zxvf yacd.tar.xz
     mkdir -p /root/.config/clash/dashboard
@@ -36,8 +34,6 @@ if [ ! -e '/root/.config/clash/dashboard/index.html' ]; then
 fi
 
 if [ ! -e '/root/.config/clash/Country.mmdb' ]; then
-    echo "已找到早期版本Country.mmdb执行删除"
-    rm -r /root/.config/clash/Country.mmdb
     echo "替换镜像的新版Country.mmdb"
     cp /tmp/Country.mmdb /root/.config/clash/Country.mmdb
 fi
