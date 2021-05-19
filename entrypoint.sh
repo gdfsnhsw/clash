d1bfda4f65955c3b10e0a8596c98faa199c21313
子 <81725003+gdfsnhsw@users.noreply.github.com>
2021-05-19 20:02:54 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,47 +2,15 @@
 # 开启转发
 sed -i "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g" /etc/sysctl.conf
 sysctl -p
-#重置iptables
-iptables -P INPUT ACCEPT
-iptables -P FORWARD ACCEPT
-iptables -P OUTPUT ACCEPT
-iptables -t nat -F
-iptables -t mangle -F
-iptables -F
-iptables -X
-#tcp
-#iptables -t nat -N clash
-#iptables -t nat -A clash -d 0.0.0.0/8 -j RETURN
-#iptables -t nat -A clash -d 10.0.0.0/8 -j RETURN
-#iptables -t nat -A clash -d 127.0.0.0/8 -j RETURN
-#iptables -t nat -A clash -d 169.254.0.0/16 -j RETURN
-#iptables -t nat -A clash -d 172.16.0.0/12 -j RETURN
-#iptables -t nat -A clash -d 192.168.0.0/16 -j RETURN
-#iptables -t nat -A clash -d 224.0.0.0/4 -j RETURN
-#iptables -t nat -A clash -d 240.0.0.0/4 -j RETURN
-#iptables -t nat -A clash -p tcp -j REDIRECT --to-port 7892
-#iptables -t nat -A PREROUTING -p tcp -j clash
-#tcp-udp
-ip rule add fwmark 1 table 100
-ip route add local default dev lo table 100
-iptables -t mangle -N clash
-iptables -t mangle -A clash -d 0.0.0.0/8 -j RETURN
-iptables -t mangle -A clash -d 10.0.0.0/8 -j RETURN
-iptables -t mangle -A clash -d 127.0.0.0/8 -j RETURN
-iptables -t mangle -A clash -d 169.254.0.0/16 -j RETURN
-iptables -t mangle -A clash -d 172.16.0.0/12 -j RETURN
-iptables -t mangle -A clash -d 192.168.0.0/16 -j RETURN
-iptables -t mangle -A clash -d 224.0.0.0/4 -j RETURN
-iptables -t mangle -A clash -d 240.0.0.0/4 -j RETURN
-iptables -t mangle -A clash -p tcp -j TPROXY --on-port 7893 --tproxy-mark 1
-iptables -t mangle -A clash -p udp -j TPROXY --on-port 7893 --tproxy-mark 1
-iptables -t mangle -A PREROUTING -j clash
+
+
+
 echo -e "======================== 1. 判断目录是否存在文件 ========================\n"
 if [ ! -e '/root/.config/clash/dashboard/index.html' ]; then
     echo "开始移动面板文件到dashboard目录"
-    tar -xvf yacd.tar.xz
+    unzip clash-dashboard-gh-pages.zip
     mkdir -p /root/.config/clash/dashboard
-    mv /public/* /root/.config/clash/dashboard
+    mv /clash-dashboard-gh-pages/* /root/.config/clash/dashboard
 fi
 
 if [ ! -e '/root/.config/clash/Country.mmdb' ]; then
